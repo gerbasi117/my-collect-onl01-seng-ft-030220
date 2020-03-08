d2630a9d9e26ab250b8d2ac664240c772b2f064e
Brandon Gerbasi <brandon.gerbasi@gmail.com>
2020-03-08 00:23:38 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,9 @@
-def my_collect(collection)
-  if block_given?
-    i = 0
- 
-    while i < collection.length
-      yield(collection[i])
-      i = i + 1
-    end
+def my_collect(array)
+  i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
 end
